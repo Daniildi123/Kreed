8571b7bc78d6395e5f317a8965de91fb19e69b23
Daniil <mvdana4@gmail.com>
2022-08-27 12:30:37 +0200

    first commit

---
+++ Laba №1 ООП/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laba__1_ООП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
