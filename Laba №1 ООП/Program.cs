2ac0837f74d47a773d335dcd74242ff193ffa4a2
Daniil <mvdana4@gmail.com>
2022-08-27 12:35:21 +0200

    second commit

--- Laba №1 ООП/Program.cs
+++ Laba №1 ООП/Program.cs
@@ -6,7 +6,7 @@ namespace Laba__1_ООП
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Egor !");
         }
     }
 }
